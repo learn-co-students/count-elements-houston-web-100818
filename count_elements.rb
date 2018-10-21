4bfd003fd0694eacc588529dc8c4d90b752aa06a
Robert Schulte <schulte.robertw@outlook.com>
2018-10-21 16:47:40 -0500

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
+require 'pry'
+
+#  returns a hash of key/value pairs in which the keys are the original string elements of the array and their associated values are a count of how many times they appeared in the array.
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do | string |
+    if new_hash[string] == nil
+      new_hash[string] = 1
+    else
+      new_hash[string] += 1
+    end
+  end
+  new_hash
 end
- 
